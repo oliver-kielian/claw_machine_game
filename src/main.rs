d616335b10ea22a2d6a0cc96e8a64c0530948f14
Oliver Kielian <oliverkielian02@gmail.com>
2024-12-10 14:39:37 -0700

    Basic Claw movement (additional directions for testing)

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,25 @@ fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
     .add_systems(Startup, setup)
+    .add_systems(Update, move_claw)
     .run();
 }
+
+#[derive(Component)]
+struct Claw;
 
 
 fn setup(
     mut commands : Commands,
     asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ){
     let text_handle_top = asset_server.load("background/topMachine.png");
     let text_handle_bottom = asset_server.load("background/bottomMachine.png");
     let text_handle_shadow_balls = asset_server.load("background/shadowBall.png");
     let text_handle_balls_group = asset_server.load("background/ballsGroup.png");
 
-    let text_handle_claw = asset_server.load("background/claws.png");
-    let layout = TextureAtlasLayout::from_grid(UVec2::new(64,128), 2, 1, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-
+    let text_handle_claw_closed = asset_server.load("background/clawClosed.png");
+   
 
     commands.spawn(Camera2dBundle{
         ..default()
@@ -67,14 +68,42 @@ fn setup(
         ..default()
     });
 
-    commands.spawn(SpriteBundle{
-        texture: text_handle_claw,
+    commands.spawn((
+        SpriteBundle{
+        texture: text_handle_claw_closed,
         sprite: Sprite {
             custom_size: Some(Vec2::new(700.0, 700.0)), 
             ..default()
         },
         transform: Transform::from_translation(Vec3::new(0.0, 0.0, -1.0)),
         ..default()
-    });
+    },
+    Claw
+    ));
 
+}
+
+
+fn move_claw(mut query: Query<&mut Transform, With<Claw>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>)
+{
+    let mut tr = query.single_mut();
+    let speed = 200.0;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft){
+        tr.translation.x -= speed * time.delta_seconds();
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowRight){
+        tr.translation.x += speed * time.delta_seconds();
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowDown){
+        tr.translation.y -= speed * time.delta_seconds();
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowUp){
+        tr.translation.y += speed * time.delta_seconds();
+    }
 }
