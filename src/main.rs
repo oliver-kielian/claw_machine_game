b933565d8e6b34ba02995c4be8d21fe82ca1799f
Oliver Kielian <oliverkielian02@gmail.com>
2024-12-13 12:43:41 -0700

    Added claw collisions

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     .add_plugins(DefaultPlugins)
     .add_systems(Startup, setup)
     .add_systems(Update, move_claw)
+    .add_systems(Update, claw_collisions)
     .run();
 }
 
@@ -30,8 +31,8 @@ fn setup(
 ){
     let text_handle_top = asset_server.load("background/topMachine.png");
     let text_handle_bottom = asset_server.load("background/bottomMachine.png");
-    let text_handle_right = asset_server.load("background/rightMachine");
-    let text_handle_left = asset_server.load("background/leftMachine");
+    let text_handle_right = asset_server.load("background/rightMachine.png");
+    let text_handle_left = asset_server.load("background/leftMachine.png");
     let text_handle_shadow_balls = asset_server.load("background/shadowBall.png");
     let text_handle_balls_group = asset_server.load("background/ballsGroup.png");
 
@@ -47,7 +48,7 @@ fn setup(
         texture: text_handle_top,
         transform: Transform {
             scale: Vec3::splat(0.2), 
-            translation: Vec3::new(0.0, 0.0, 1.0),
+            translation: Vec3::new(0.0, 280.0, 3.0),
             ..default()
         },
         ..default()
@@ -59,7 +60,7 @@ fn setup(
         texture: text_handle_shadow_balls,
         transform: Transform {
             scale: Vec3::splat(0.2), 
-            translation: Vec3::new(0.0, 0.0, -2.0),
+            translation: Vec3::new(0.0, -45.0, -2.0),
             ..default()
         },
         ..default()
@@ -70,7 +71,7 @@ fn setup(
         texture: text_handle_balls_group,
         transform: Transform {
             scale: Vec3::splat(0.2), 
-            translation: Vec3::new(0.0, 0.0, 1.0),
+            translation: Vec3::new(0.0, -45.0, 1.0),
             ..default()
         },
         ..default()
@@ -81,7 +82,7 @@ fn setup(
         texture: text_handle_left,
         transform: Transform {
             scale: Vec3::splat(0.2), 
-            translation: Vec3::new(0.0, 0.0, 1.0),
+            translation: Vec3::new(-245.0, 35.0, 2.0),
             ..default()
         },
         ..default()
@@ -92,7 +93,7 @@ fn setup(
         texture: text_handle_right,
         transform: Transform {
             scale: Vec3::splat(0.2), 
-            translation: Vec3::new(0.0, 0.0, 1.0),
+            translation: Vec3::new(247.0, 30.0, 2.0),
             ..default()
         },
         ..default()
@@ -104,7 +105,7 @@ fn setup(
         texture: text_handle_bottom,
         transform: Transform {
             scale: Vec3::splat(0.2), 
-            translation: Vec3::new(0.0, -200.0, 1.0),
+            translation: Vec3::new(0.0, -220.0, 1.0),
             ..default()
         },
         ..default()
@@ -115,7 +116,7 @@ fn setup(
         texture: text_handle_claw_closed,
         transform: Transform {
             scale: Vec3::splat(0.2), 
-            translation: Vec3::new(0.0, 0.0, -1.0),
+            translation: Vec3::new(0.0, 200.0, -1.0),
             ..default()
         },
         ..default()
@@ -149,11 +150,31 @@ fn move_claw(mut query: Query<&mut Transform, With<Claw>>,
         tr.translation.y += speed * time.delta_seconds();
     }
 }
-/* 
-pub clawColisions(
-    mut claw_query: Query<(Entity, &Transform), With<Claw>>,
-    mut top_query: Query<&Transform, With<TopMachine>>,
-    mut sides_query: Query<&Transform, With<BottomMachine>>,
-    mut balls_query: Query<&Transform, with<BallsGroup>>
-)
-*/
+
+pub fn claw_collisions(
+    mut claw_query: Query<(Entity, &mut Transform), With<Claw>>,
+){
+    if let Ok((claw_entity, mut claw_transform)) = claw_query.get_single_mut(){
+        let x_max = 150.0;
+        let x_min = -150.0;
+        let y_max = 200.0;
+        let y_min = 60.0;
+
+        let mut translation = claw_transform.translation;
+
+        if translation.x < x_min{
+            translation.x = x_min
+        } else if translation.x > x_max{
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min{
+            translation.y = y_min;
+        } else if translation.y > y_max
+        {
+            translation.y = y_max;
+        }
+
+        claw_transform.translation = translation;
+    }
+}
