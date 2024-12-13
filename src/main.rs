4e8a01e4986e498d605f18d0c08d5b981a728879
Oliver Kielian <oliverkielian02@gmail.com>
2024-12-13 14:21:37 -0700

    Added claw up and down movement and updated resources

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,44 @@
 use bevy::prelude::*;
 
+const X_MAX: f32 = 150.0;
+const X_MIN: f32 = -150.0;
+const Y_MAX: f32 = 200.0;
+const Y_MIN: f32 = 60.0;
+const SPEED: f32 = 200.0;
+
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
+     .insert_resource(ClawState::default())
     .add_systems(Startup, setup)
     .add_systems(Update, move_claw)
     .add_systems(Update, claw_collisions)
+    .add_systems(Update, drop_claw)
+    .add_systems(Update, raise_claw)
+    .add_systems(Update, open_claw)
+    .add_systems(Update, close_claw)
     .run();
 }
 
 #[derive(Component)]
-struct Claw;
+pub struct Claw;
+
+#[derive(Resource)]
+pub struct ClawState {
+    is_moving: bool,
+    down: bool,
+    up: bool
+}
+
+impl Default for ClawState {
+    fn default() -> Self {
+        ClawState {
+            is_moving: false,
+            down: false,
+            up: true,
+        }
+    }
+}
 
 #[derive(Component)]
 struct BallsGroup;
@@ -23,6 +51,7 @@ struct RightMachine;
 
 #[derive(Component)]
 struct LeftMachine;
+
 
 
 fn setup(
@@ -132,49 +161,155 @@ fn move_claw(mut query: Query<&mut Transform, With<Claw>>,
     time: Res<Time>)
 {
     let mut tr = query.single_mut();
-    let speed = 200.0;
 
     if keyboard_input.pressed(KeyCode::ArrowLeft){
-        tr.translation.x -= speed * time.delta_seconds();
+        tr.translation.x -= SPEED * time.delta_seconds();
     }
 
     if keyboard_input.pressed(KeyCode::ArrowRight){
-        tr.translation.x += speed * time.delta_seconds();
+        tr.translation.x += SPEED * time.delta_seconds();
     }
 
     if keyboard_input.pressed(KeyCode::ArrowDown){
-        tr.translation.y -= speed * time.delta_seconds();
+        tr.translation.y -= SPEED * time.delta_seconds();
     }
 
     if keyboard_input.pressed(KeyCode::ArrowUp){
-        tr.translation.y += speed * time.delta_seconds();
+        tr.translation.y += SPEED * time.delta_seconds();
     }
 }
 
 pub fn claw_collisions(
     mut claw_query: Query<(Entity, &mut Transform), With<Claw>>,
 ){
-    if let Ok((claw_entity, mut claw_transform)) = claw_query.get_single_mut(){
-        let x_max = 150.0;
-        let x_min = -150.0;
-        let y_max = 200.0;
-        let y_min = 60.0;
+    if let Ok((_claw_entity, mut claw_transform)) = claw_query.get_single_mut(){
 
         let mut translation = claw_transform.translation;
 
-        if translation.x < x_min{
-            translation.x = x_min
-        } else if translation.x > x_max{
-            translation.x = x_max;
+        if translation.x < X_MIN{
+            translation.x = X_MIN
+        } else if translation.x > X_MAX{
+            translation.x = X_MAX;
         }
 
-        if translation.y < y_min{
-            translation.y = y_min;
-        } else if translation.y > y_max
+        if translation.y < Y_MIN{
+            translation.y = Y_MIN;
+        } else if translation.y > Y_MAX
         {
-            translation.y = y_max;
+            translation.y = Y_MAX;
         }
 
         claw_transform.translation = translation;
     }
+}
+
+pub fn drop_claw(
+    mut query: Query<(&mut Transform, &Claw)>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut claw_state: ResMut<ClawState>
+)
+{
+    if keyboard_input.just_pressed(KeyCode::Space) && claw_state.up
+    {
+        claw_state.is_moving = true;
+    }
+    if claw_state.is_moving && claw_state.up
+    {
+    for(mut transform, _claw) in query.iter_mut(){
+        transform.translation.y -= SPEED * time.delta_seconds();
+        if transform.translation.y <= Y_MIN{
+            transform.translation.y = Y_MIN;
+            claw_state.is_moving = false;
+            claw_state.down = true;
+            claw_state.up = false;
+        }
+    }
+}
+}
+
+pub fn raise_claw(
+    mut query: Query<(&mut Transform, &Claw)>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut claw_state: ResMut<ClawState>
+){
+    if keyboard_input.just_pressed(KeyCode::Space) && claw_state.down
+    {
+        claw_state.is_moving = true;
+    }
+    if claw_state.is_moving && claw_state.down
+    {
+    for(mut transform, _claw) in query.iter_mut(){
+        transform.translation.y += SPEED * time.delta_seconds();
+        if transform.translation.y >= Y_MAX{
+            transform.translation.y = Y_MAX;
+            claw_state.is_moving = false;
+            claw_state.down = false;
+            claw_state.up = true;
+        }
+    }
+}
+}
+
+pub fn open_claw(
+    mut commands : Commands,
+    mut claw_query: Query<(Entity, &Transform), With<Claw>>,
+    asset_server: Res<AssetServer>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    claw_state: Res<ClawState>
+){
+    if keyboard_input.just_pressed(KeyCode::Space) && claw_state.up{
+    if let Ok((claw_entity, claw_transform)) = claw_query.get_single_mut(){
+        let translation = claw_transform.translation;
+        let text_handle_open = asset_server.load("background/clawOpen.png");
+
+        commands.spawn((
+            SpriteBundle{
+                texture: text_handle_open,
+                transform: Transform {
+                    scale: Vec3::splat(0.2), 
+                    translation,
+                    ..default()
+                },
+                ..default()
+            },
+            Claw
+        ));
+
+        commands.entity(claw_entity).despawn();
+
+    }
+}
+}
+
+pub fn close_claw(
+    mut commands : Commands,
+    mut claw_query: Query<(Entity, &Transform), With<Claw>>,
+    asset_server: Res<AssetServer>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    claw_state: Res<ClawState>
+){
+    if keyboard_input.just_pressed(KeyCode::Space) && claw_state.down{
+    if let Ok((claw_entity, claw_transform)) = claw_query.get_single_mut(){
+        let translation = claw_transform.translation;
+        let text_handle_close = asset_server.load("background/clawClosed.png");
+
+        commands.spawn((
+            SpriteBundle{
+                texture: text_handle_close,
+                transform: Transform {
+                    scale: Vec3::splat(0.2), 
+                    translation,
+                    ..default()
+                },
+                ..default()
+            },
+            Claw
+        ));
+
+        commands.entity(claw_entity).despawn();
+
+    }
+}
 }
