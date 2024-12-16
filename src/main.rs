2d427e11ef379d093a928146045742d6fc7b8168
Oliver Kielian <oliverkielian02@gmail.com>
2024-12-16 13:57:20 -0700

    Added help text

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,8 @@ fn main() {
     .add_systems(Update, win_cat)
     .add_systems(Update, depawn_cat)
     .add_systems(Update, animate_cat)
+    .add_systems(Startup, move_claw_text)
+    .add_systems(Update, despawn_help_text)
     .run();
 }
 
@@ -107,8 +109,10 @@ pub struct CatIndices{
 pub struct AnimationTimer(Timer);
 
 #[derive(Component)]
-pub struct HelpText;
+pub struct HelpTextLeftRight;
 
+#[derive(Component)]
+pub struct HelpTextSpace;
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let text_handle_top = asset_server.load("background/topMachine.png");
     let text_handle_bottom = asset_server.load("background/bottomMachine.png");
@@ -210,6 +214,66 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     },
         Claw,
     ));
+}
+
+pub fn move_claw_text(
+    mut commands: Commands
+){
+    commands.spawn((Text2dBundle{
+        text: Text::from_section("Press the left arrow\nto move the claw to the left\n\nPress the right arrow\nto move the claw to the right", 
+    TextStyle::default()),
+        transform: Transform{
+            translation: Vec3::new(-500., 300., 1.),
+            ..default()
+        },
+      ..default()
+    },
+    HelpTextLeftRight
+    ));
+
+    commands.spawn((Text2dBundle{
+    text: Text::from_section("Press Space\nto drop and raise the claw", TextStyle::default()),
+    transform: Transform{
+        translation: Vec3::new(-500., 100., 1.),
+        ..default()
+    },
+    ..default()
+    },
+    HelpTextSpace
+    ));
+
+    commands.spawn((Text2dBundle{
+        text: Text::from_section("You have anywhere from 1 to 5 seconds\nto drop the ball into the gift return\n Good Luck!", 
+    TextStyle::default()),
+    transform: Transform{
+        translation: Vec3::new(-500., -100., 1.),
+        ..default()
+    },
+    ..default()
+    },
+    HelpTextSpace
+    ));
+
+}
+
+
+pub fn despawn_help_text(
+    mut commands: Commands,
+    query_side: Query<Entity, With<HelpTextLeftRight>>,
+    query_vertical: Query<Entity, With<HelpTextSpace>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>
+){
+    if keyboard_input.just_pressed(KeyCode::Space){
+        for text in query_vertical.iter() {
+            commands.entity(text).despawn();
+        }
+    }
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::ArrowLeft){
+        for text in query_side.iter() {
+            commands.entity(text).despawn();
+        }
+    }
 }
 
 fn move_claw(
